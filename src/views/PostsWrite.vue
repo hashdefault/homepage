1ad032405ad504771b7cd933edb8d5295c7b45f8
Lucas G. Bueno <lucasgbueno@proton.me>
2025-01-16 16:10:57 -0300

    add novo post

--- src/views/PostsWrite.vue
+++ src/views/PostsWrite.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="description" id='ref_id'>
     <article class="art">
+      <h3>ano 2024, 16 de janeiro</h3>
+      <p>
+      Computação em nuvem tem sido minha prioridade para as tecnologias que quero aprender esse ano. 
+      Comecei pela AWS, já aprendi vários conceitos e dinâmicas novas de deploy e configurações de servidor. Mesmo estando por dentro de
+      várias das ideias dessa área pude notar que ainda tenho muito o que explorar. Alguns dos serviços que ja tive contato foram EC2, para criação
+      e configuração de VPS, Route 53 para criação de hosted zones, Cloudfront para distribuição, Amazon S3 buckets para armazenamento de dados, AWS Certificate
+      Manager para emissão de certificados SSL.
+      Fiz o deploy do meu site pessoal usando essas tecnologias e decidi fazer utilizando cada serviço e configurando eu mesmo pra não pular etapas
+      e entender realmente como um projeto funciona, desde a fundação mais básica até o momento em que digitamos a url no browser.
+      </p>
+    </article>
+ 
+    <article class="art">
       <h3>ano 2024, 09 de janeiro</h3>
       <p>
         Para este ano planejei e tracei algumas metas e objetivos mais
