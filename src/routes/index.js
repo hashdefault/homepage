85ae70521e9daa5d17477579b686091082179b01
Lucas G. Bueno <lucasgbueno@proton.me>
2025-01-21 17:56:01 -0300

    altera hash history para webhistory

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import HomePage from './../views/HomePage.vue'
 import PostsWrite from './../views/PostsWrite.vue'
 import VideosPage from './../views/VideosPage.vue'
@@ -13,7 +13,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
